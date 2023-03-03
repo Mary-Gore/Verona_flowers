a1e96230bf5ba65446085782d5fca45f588d82e7
Mary Gore <dm101mary@gmail.com>
2023-03-03 16:23:10 +0400

    Обновлен каталог

--- script/script.js
+++ script/script.js
@@ -9,16 +9,11 @@ const countCatalog = () => {
       spanCalc = countItem.querySelector('.btns-calc-wrapper span'),
       price = countItem.querySelector('.wrapper-info span'),
       startPrice = parseFloat(price.textContent),
+      calcPopup = countItem.querySelector('.calc-popup'),
+      total = calcPopup.querySelector('.total span'),
+      checkboxes = calcPopup.querySelectorAll('input[type="checkbox"]'),
+      btnClose = countItem.querySelector('.icon-close'),
       btnBuy = countItem.querySelector('.btns-wrapper > button');
-
-
-
-    /* const total = calcWrapper.querySelector('.total span'),
-     checkboxes = calcWrapper.querySelectorAll('input[type="checkbox"]'),
-           btnClose = countItem.querySelector('.icon-close'),
-                 calcWrapper = countItem.querySelector('.wrapper-calc-form'),
-    */
-
 
     plusBtn.addEventListener('click', () => {
       count++;
@@ -35,35 +30,35 @@ const countCatalog = () => {
       spanCalc.textContent = count;
     });
 
-    /*  btnBuy.addEventListener('click', () => {
-        calcWrapper.style.cssText = `
+    btnBuy.addEventListener('click', () => {
+      calcPopup.style.cssText = `
       display:flex;
       flex-direction: column;
     `;
-  
-        total.textContent = `${parseFloat(price.textContent)} ₽`;
-  
-        checkboxes.forEach(checkbox => {
-          checkbox.addEventListener('change', () => {
-            if (checkbox.checked) {
-              total.textContent = `${parseFloat(total.textContent) + parseFloat(checkbox.value)} ₽`;
-            } else {
-              total.textContent = `${parseFloat(total.textContent) - parseFloat(checkbox.value)} ₽`;
-            }
-          });
-        });
-  
-      }); */
 
-    /*  btnClose.addEventListener('click', () => {
-        calcWrapper.style.display = 'none';
-  
-        checkboxes.forEach(checkbox => {
+      total.textContent = `${parseFloat(price.textContent)} ₽`;
+
+      checkboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', () => {
           if (checkbox.checked) {
-            checkbox.checked = false;
+            total.textContent = `${parseFloat(total.textContent) + parseFloat(checkbox.value)} ₽`;
+          } else {
+            total.textContent = `${parseFloat(total.textContent) - parseFloat(checkbox.value)} ₽`;
           }
         });
-      }); */
+      });
+
+    });
+
+    btnClose.addEventListener('click', () => {
+      calcPopup.style.display = 'none';
+
+      checkboxes.forEach(checkbox => {
+        if (checkbox.checked) {
+          checkbox.checked = false;
+        }
+      });
+    });
 
   });
 
