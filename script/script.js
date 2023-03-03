f2f39a4f3eb5d97ce7c97f738f10d50693ca28ac
Mary Gore <dm101mary@gmail.com>
2023-03-03 17:11:38 +0400

    Добавлены стили аниации

--- script/script.js
+++ script/script.js
@@ -10,7 +10,7 @@ const countCatalog = () => {
       price = countItem.querySelector('.wrapper-info span'),
       startPrice = parseFloat(price.textContent),
       calcPopup = countItem.querySelector('.calc-popup'),
-      total = calcPopup.querySelector('.total span'),
+      total = calcPopup.querySelector('.total > span'),
       checkboxes = calcPopup.querySelectorAll('input[type="checkbox"]'),
       btnClose = countItem.querySelector('.icon-close'),
       btnBuy = countItem.querySelector('.btns-wrapper > button');
@@ -31,11 +31,6 @@ const countCatalog = () => {
     });
 
     btnBuy.addEventListener('click', () => {
-      calcPopup.style.cssText = `
-      display:flex;
-      flex-direction: column;
-    `;
-
       total.textContent = `${parseFloat(price.textContent)} ₽`;
 
       checkboxes.forEach(checkbox => {
@@ -66,7 +61,74 @@ const countCatalog = () => {
 
 }
 
-countCatalog();
+//countCatalog();
+
+const popupAnimate = () => {
+  const dataPopupsBtns = document.querySelectorAll('[data-simple-popup]');
+
+  if (dataPopupsBtns.length === 0) return;
+
+  const overlay = document.querySelector('.overlay'),
+    popups = document.querySelectorAll('.popup'),
+    dataClose = document.querySelectorAll('[data-closed]');
+
+  const modalShow = popup => {
+    if (window.innerWidth > 992) {
+      overlay.classList.remove('fadeOut');
+      overlay.classList.add('fadeIn');
+
+      popup.classList.remove('fadeOut');
+      popup.classList.add('fadeIn');
+    } else {
+      popup.classList.remove('hide-mobile');
+      popup.classList.add('show-mobile');
+      overlay.style.visibillity = 'visible';
+      overlay.style.opacity = '1';
+    }
+
+    popup.classList.add('is-open');
+  };
+
+  const modalClose = e => {
+    for (let popupElem of popups) {
+      if (popupElem.classList.contains('is-open') && (e.type !== 'keydown' || e.keyCode === 27)) {
+        if (window.innerWidth > 992) {
+          popupElem.classList.remove('fadeIn');
+          popupElem.classList.add('fadeOut');
+        }
+      } else {
+        popupElem.classList.remove('show-mobile');
+        popupElem.classList.add('hide-mobile');
+        overlay.style.visibillity = 'hidden';
+        overlay.style.opacity = '0';
+      }
+
+      if (window.innerWidth > 992) {
+        overlay.classList.remove('fadeIn');
+        overlay.classList.add('fadeOut');
+      }
+
+      popupElem.classList.remove('is-open');
+    }
+  };
+
+  for (let elem of dataPopupsBtns) {
+    elem.addEventListener('click', () => {
+      let popup = document.getElementById(elem.dataset.simplePopup);
+      console.log('popup: ', popup);
+      modalShow(popup);
+    });
+  }
+
+  for (let elem of dataClose) {
+    elem.addEventListener('click', modalClose);
+  }
+
+  document.addEventListener('keydown', modalClose);
+};
+
+popupAnimate();
+
 
 
 
