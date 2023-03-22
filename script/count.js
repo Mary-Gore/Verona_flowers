e02665fa9bd9c7817708da79c5b73999eef138e9
Mary Gore <dm101mary@gmail.com>
2023-03-23 02:57:20 +0400

    Доделан счётчик корзины

--- script/count.js
+++ script/count.js
@@ -10,20 +10,31 @@ const count = () => {
     }
 
     if (e.target.dataset.action === 'minus') {
-      if (parseInt(countNum.textContent) > 1) {
-        countNum.textContent = --countNum.textContent;
-      } else if (e.target.closest('.cart-wrapper') && parseInt(countNum.textContent) === 1) {
-        e.target.closest('.cart-item').remove();
-
-        calcPrice();
-        toggleCartStatus();
-      } 
+      if (!e.target.closest('.cart-wrapper')) {
+        if (parseInt(countNum.textContent) > 1) {
+          countNum.textContent = --countNum.textContent;
+        }
+      } else {
+        if (parseInt(countNum.textContent) > 1) {
+          countNum.textContent = --countNum.textContent;
+          cartCounter.textContent = --cartCounter.textContent;
+        } else if (parseInt(countNum.textContent) === 1) {
+          e.target.closest('.cart-item').remove();
+          
+          calcPrice();
+          toggleCartStatus();
+        }
+      }
     }
+
 
     if (e.target.dataset.action === 'plus') {
       countNum.textContent = ++countNum.textContent;
-    }
 
+      if (e.target.closest('.cart-wrapper')) {
+        cartCounter.textContent = ++cartCounter.textContent;
+      }
+    }
     // Проверка на клик +- в корзине
     if (e.target.hasAttribute('data-action') && e.target.closest('.cart-wrapper')) {
       // Пересчёт общей стоимости в корзине
