fb09201409651732a87d2443dd4198b3182e66db
Mary Gore <dm101mary@gmail.com>
2023-03-31 13:35:58 +0400

    Добавлено закрытие корзины по клику вне корзины

--- script/eventListeners.js
+++ script/eventListeners.js
@@ -1,4 +1,6 @@
 const eventListeners = () => {
+  const cart = document.querySelector('.cart');
+
   document.body.addEventListener('click', e => {
     if (e.target.id === 'cart_btn') {
       document.querySelector('.cart').style.display = 'block';
@@ -6,6 +8,15 @@ const eventListeners = () => {
 
     if (e.target.closest('.cart-close')) {
       document.querySelector('.cart').style.display = 'none';
+    }
+  });
+
+  cart.addEventListener('click', e => {
+    let target = e.target;
+    target = target.closest('.cart-body');
+
+    if (!target) {
+     cart.style.display = 'none';
     }
   });
 };
